7e44ea0dd33d4d46c05064df5cbefb05337fac62
NachinBombin <solidnachox@gmail.com>
2026-03-25 02:04:09 -0300

    Add files via upload

---
+++ autorun/mechassault_autorun.lua
@@ -0,0 +1,191 @@
+local vector = FindMetaTable("Vector")
+local angle = FindMetaTable("Angle")
+
+function vector:Approach(target, rate)
+	local diff = target - self
+	local ratio = diff:GetNormalized()
+
+	self.x = math.Approach(self.x, target.x, rate * ratio.x)
+	self.y = math.Approach(self.y, target.y, rate * ratio.y)
+	self.z = math.Approach(self.z, target.z, rate * ratio.z)
+end
+
+function vector:Clamp(min, max)
+	self.x = math.Clamp(self.x, min.x, max.x)
+	self.y = math.Clamp(self.y, min.y, max.y)
+	self.z = math.Clamp(self.z, min.z, max.z)
+end
+
+function angle:Approach(target, rate)
+	self.p = math.ApproachAngle(self.p, target.p, rate)
+	self.y = math.ApproachAngle(self.y, target.y, rate)
+	self.r = math.ApproachAngle(self.r, target.r, rate)
+end
+
+function scripted_ents.IsTypeOf(name, base)
+	return name == base or scripted_ents.IsBasedOn(name, base)
+end
+
+debugTime = engine.TickInterval() * 2
+
+include("mechassault/sh_sound.lua")
+
+game.AddParticles("particles/gm_mechassault_2_projectile_effects.pcf")
+game.AddParticles("particles/gm_mechassault_2_muzzleflash_effects.pcf")
+game.AddParticles("particles/gm_mechassault_2_explosions.pcf")
+game.AddParticles("particles/gm_mechassault_2_ballistic_tracers.pcf")
+game.AddParticles("particles/gm_mechassault_2_jumpjet_effects.pcf")
+game.AddParticles("particles/gm_mechassault_2_machinegun_impact.pcf")
+
+PrecacheParticleSystem("gm_MA2_VTOL_Contrail")
+
+hook.Add("SetupMove", "mechassault", function(ply, mv, cmd)
+	local ent = ply:GetNWEntity("mechassault")
+
+	if not IsValid(ent) then
+		return
+	end
+
+	if CLIENT and not ent:GetPredictable() then
+		ent:SetPredictable(true)
+	end
+
+	if ent:AllowInput() and ent:StartMove(ply, mv, cmd) then
+		ent.PlayerExiting = true
+	end
+end)
+
+hook.Add("VehicleMove", "mechassault", function(ply, vehicle, mv)
+	local ent = ply:GetNWEntity("mechassault")
+
+	if not IsValid(ent) then
+		return
+	end
+
+	if ent:AllowInput() then
+		ent:Move(mv)
+	end
+
+	return true
+end)
+
+hook.Add("FinishMove", "mechassault", function(ply, mv)
+	local ent = ply:GetNWEntity("mechassault")
+
+	if not IsValid(ent) then
+		return
+	end
+
+	if ent:AllowInput() then
+		ent:FinishMove(mv)
+
+		if ent.PlayerExiting then
+			ent:StopDriving()
+			ent.PlayerExiting = nil
+		end
+	end
+
+	return true
+end)
+
+if CLIENT then
+	net.Receive("nMAStopEffect", function(len)
+		local ent = net.ReadEntity()
+
+		if IsValid(ent) then
+			ent:StopParticlesNamed(net.ReadString())
+		end
+	end)
+
+	net.Receive("nMAStopObs", function(len)
+		local ply = net.ReadEntity()
+
+		if IsValid(ply) then
+			ply:SetObserverMode(OBS_MODE_NONE)
+		end
+	end)
+
+	hook.Add("CreateMove", "mechassault", function(cmd)
+		local ent = LocalPlayer():GetNWEntity("mechassault")
+
+		if not IsValid(ent) then
+			return
+		end
+
+		for i = 0, 9 do
+			local enum = "KEY_" .. i
+
+			if input.IsKeyDown(_G[enum]) then
+				cmd:SetImpulse(i)
+
+				break
+			end
+		end
+	end)
+
+	hook.Add("CalcVehicleView", "mechassault", function(vehicle, ply, view)
+		local ent = ply:GetNWEntity("mechassault")
+
+		if not IsValid(ent) then
+			return
+		end
+
+		if ply:GetViewEntity() != ply then
+			ply:SetObserverMode(OBS_MODE_NONE)
+		else
+			ply:SetObserverMode(OBS_MODE_CHASE)
+		end
+
+		ent:CalcView(ply, view)
+	end)
+
+	hook.Add("PreDrawHUD", "mechassault", function()
+		local ent = LocalPlayer():GetNWEntity("mechassault")
+
+		if not IsValid(ent) then
+			return
+		end
+
+		ent:DrawHUD()
+	end)
+
+	hook.Add("PrePlayerDraw", "mechassault", function(ply)
+		local ent = ply:GetNWEntity("mechassault")
+
+		if IsValid(ent) then
+			return true
+		end
+	end)
+else
+	util.AddNetworkString("nMAStopEffect")
+	util.AddNetworkString("nMAStopObs")
+
+	hook.Add("EntityTakeDamage", "mechassault", function(ply, dmg)
+		if IsValid(ply) and ply:IsPlayer() and IsValid(ply:GetNWEntity("mechassault")) then
+			return true
+		end
+	end)
+
+	hook.Add("PlayerDeath", "mechassault", function(ply)
+		local ent = ply:GetNWEntity("mechassault")
+
+		if not IsValid(ent) then
+			return
+		end
+
+		ent:StopDriving()
+		ent:EjectPlayer()
+	end)
+
+	hook.Add("CanPlayerEnterVehicle", "mechassault", function(ply, vehicle, role)
+		if vehicle.MAMech and not vehicle.MAMech.AllowEnter then
+			return false
+		end
+	end)
+
+	hook.Add("CanExitVehicle", "mechassault", function(vehicle, ply)
+		if vehicle.MAMech then
+			return false
+		end
+	end)
+end
---
+++ mechassault/sh_sound.lua
@@ -0,0 +1,223 @@
+AddCSLuaFile()
+
+hook.Add("EntityEmitSound", "mechassault", function(snd)
+	if snd.OriginalSoundName == "MA2_Mech.Step" then
+		snd.SoundName = string.format("mechassault_2/mechs/mech_step_%s.ogg", math.random(1, 4)) -- We just want something random, not mech_step_1 through 4 in order
+
+		return true
+	end
+end)
+
+sound.Add({
+	name = "MA2_Mech.Step",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = {
+		Sound("mechassault_2/mechs/mech_step_1.ogg"),
+		Sound("mechassault_2/mechs/mech_step_2.ogg"),
+		Sound("mechassault_2/mechs/mech_step_3.ogg"),
+		Sound("mechassault_2/mechs/mech_step_4.ogg")
+	}
+})
+
+sound.Add({
+	name = "MA2_BA.Step",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/battlearmor/battlearmor_step.ogg")
+})
+
+sound.Add({
+	name = "MA2_Mech.JJStart",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/jumpjets_start.ogg")
+})
+
+sound.Add({
+	name = "MA2_Mech.JJLoop",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/jumpjets_loop.wav")
+})
+
+sound.Add({
+	name = "MA2_Mech.JJEnd",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/jumpjets_end.ogg")
+})
+
+sound.Add({
+	name = "MA2_Mech.BJJStart",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/battlearmor/jumpjets_start.wav")
+})
+
+sound.Add({
+	name = "MA2_Mech.BJJLoop",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/battlearmor/jumpjets_loop.wav")
+})
+
+sound.Add({
+	name = "MA2_Mech.BJJEnd",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/mechs/battlearmor/jumpjets_stop.wav")
+})
+
+sound.Add({
+	name = "MA2_Misc.Salvage",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 100,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/items/salvage.ogg")
+})
+
+local sounds = {
+	{"MA2_Weapon.Autocannon1", Sound("mechassault_2/weapons/autocannon_lvl1.ogg")},
+	{"MA2_Weapon.Autocannon2", Sound("mechassault_2/weapons/autocannon_lvl2.ogg")},
+	{"MA2_Weapon.Autocannon3", Sound("mechassault_2/weapons/autocannon_lvl3.ogg")},
+	{"MA2_Weapon.Crossbow1", Sound("mechassault_2/weapons/crossbow_lvl1.ogg")},
+	{"MA2_Weapon.Crossbow2", Sound("mechassault_2/weapons/crossbow_lvl2.ogg")},
+	{"MA2_Weapon.Crossbow3", Sound("mechassault_2/weapons/crossbow_lvl3.ogg")},
+	{"MA2_Weapon.Flame1", Sound("mechassault_2/weapons/flamer_lvl1.ogg")},
+	{"MA2_Weapon.Flame2", Sound("mechassault_2/weapons/flamer_lvl2.ogg")},
+	{"MA2_Weapon.Flame3", Sound("mechassault_2/weapons/flamer_lvl3.ogg")},
+	{"MA2_Weapon.Gauss1", Sound("mechassault_2/weapons/gauss_lvl1.ogg")},
+	{"MA2_Weapon.Gauss2", Sound("mechassault_2/weapons/gauss_lvl2.ogg")},
+	{"MA2_Weapon.Gauss3", Sound("mechassault_2/weapons/gauss_lvl3.ogg")},
+	{"MA2_Weapon.Hammer1", Sound("mechassault_2/weapons/hammer_lvl1.ogg")},
+	{"MA2_Weapon.Hammer2", Sound("mechassault_2/weapons/hammer_lvl2.ogg")},
+	{"MA2_Weapon.Hammer3", Sound("mechassault_2/weapons/hammer_lvl3.ogg")},
+	{"MA2_Weapon.Javelin1", Sound("mechassault_2/weapons/javelin_lvl1.ogg")},
+	{"MA2_Weapon.Javelin2", Sound("mechassault_2/weapons/javelin_lvl2.ogg")},
+	{"MA2_Weapon.Javelin3", Sound("mechassault_2/weapons/javelin_lvl3.ogg")},
+	{"MA2_Weapon.Laser1", Sound("mechassault_2/weapons/laser_lvl1.ogg")},
+	{"MA2_Weapon.Laser2", Sound("mechassault_2/weapons/laser_lvl2.ogg")},
+	{"MA2_Weapon.Laser3", Sound("mechassault_2/weapons/laser_lvl3.ogg")},
+	{"MA2_Weapon.Machinegun1", Sound("mechassault_2/weapons/machinegun_lvl1.ogg")},
+	{"MA2_Weapon.Machinegun2", Sound("mechassault_2/weapons/machinegun_lvl2.ogg")},
+	{"MA2_Weapon.Machinegun3", Sound("mechassault_2/weapons/machinegun_lvl3.ogg")},
+	{"MA2_Weapon.Mortar1", Sound("mechassault_2/weapons/mortar_lvl1.ogg")},
+	{"MA2_Weapon.Mortar2", Sound("mechassault_2/weapons/mortar_lvl2.ogg")},
+	{"MA2_Weapon.Mortar3", Sound("mechassault_2/weapons/mortar_lvl3.ogg")},
+	{"MA2_Weapon.PlasmaPPC1", Sound("mechassault_2/weapons/ppc_lvl3.ogg")},
+	{"MA2_Weapon.PlasmaPPC2", Sound("mechassault_2/weapons/ppc_lvl3.ogg")},
+	{"MA2_Weapon.PlasmaPPC3", Sound("mechassault_2/weapons/ppc_lvl3.ogg")},
+	{"MA2_Weapon.PPC1", Sound("mechassault_2/weapons/ppc_lvl1.ogg")},
+	{"MA2_Weapon.PPC2", Sound("mechassault_2/weapons/ppc_lvl2.ogg")},
+	{"MA2_Weapon.PPC3", Sound("mechassault_2/weapons/ppc_lvl3.ogg")},
+	{"MA2_Weapon.PPCChargeLoop", {Sound("mechassault_2/weapons/ppc_charge_loop_1.ogg"), Sound("mechassault_2/weapons/ppc_charge_loop_2.ogg")}, CHAN_AUTO},
+	{"MA2_Weapon.PPCCharging", {Sound("mechassault_2/weapons/ppc_charge.ogg"), Sound("mechassault_2/weapons/ppc_charge_1.ogg"), Sound("mechassault_2/weapons/ppc_charge_2.ogg")}},
+	{"MA2_Weapon.PulseLaser1", Sound("mechassault_2/weapons/pulse_laser_lvl1.ogg")},
+	{"MA2_Weapon.PulseLaser2", Sound("mechassault_2/weapons/pulse_laser_lvl2.ogg")},
+	{"MA2_Weapon.PulseLaser3", Sound("mechassault_2/weapons/pulse_laser_lvl3.ogg")}
+}
+
+for _, v in pairs(sounds) do
+	sound.Add({
+		name = v[1],
+		channel = v[3] or CHAN_WEAPON,
+		volume = 1,
+		level = 140,
+		pitch = {95, 110},
+		sound = v[2]
+	})
+end
+
+sound.Add({
+	name = "MA2_Weapon.MissileHit",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 140,
+	pitch = {95, 110},
+	sound = {
+		Sound("mechassault_2/weapons/explosion_generic_1.ogg"),
+		Sound("mechassault_2/weapons/explosion_generic_2.ogg"),
+		Sound("mechassault_2/weapons/explosion_generic_3.ogg"),
+		Sound("mechassault_2/weapons/explosion_generic_4.ogg")
+	}
+})
+
+sound.Add({
+	name = "MA2_Weapon.HammerImpact",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 90,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/weapons/hammer_impact.ogg")
+})
+
+sound.Add({
+	name = "MA2_Weapon.HammerExplode",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 140,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/weapons/hammer_explode.ogg")
+})
+
+sound.Add({
+	name = "MA2_Weapon.MortarHit",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 140,
+	pitch = {95, 110},
+	sound = {
+		Sound("mechassault_2/weapons/mortar_impact_1.ogg"),
+		Sound("mechassault_2/weapons/mortar_impact_2.ogg")
+	}
+})
+
+sound.Add({
+	name = "MA2_Weapon.LaserHit",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 90,
+	pitch = {75, 90},
+	sound = Sound("mechassault_2/weapons/laser_impact_mech.ogg")
+})
+
+sound.Add({
+	name = "MA2_Weapon.AutocannonHit",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 90,
+	pitch = {95, 110},
+	sound = {
+		Sound("mechassault_2/weapons/bullet_ric_4.ogg"),
+		Sound("mechassault_2/weapons/bullet_ric_11.ogg"),
+		Sound("mechassault_2/weapons/bullet_ric_17.ogg"),
+		Sound("mechassault_2/weapons/bullet_ric_18.ogg")
+	}
+})
+
+sound.Add({
+	name = "MA2_Weapon.PPCHit",
+	channel = CHAN_AUTO,
+	volume = 1,
+	level = 90,
+	pitch = {95, 110},
+	sound = Sound("mechassault_2/weapons/ppc_impact.ogg")
+})
